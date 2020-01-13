343c033297c4760f798dcd3d69a9425763a34318
xuhj <996188227@qq.com>
2020-01-13 09:35:31 +0800

    second commit

--- src/main/java/ciih/dsg/xhj/config/MybatisPlusConfig.java
+++ src/main/java/ciih/dsg/xhj/config/MybatisPlusConfig.java
@@ -29,58 +29,63 @@ import java.util.ArrayList;
 @MapperScan("ciih.dsg.xhj.mapper")
 public class MybatisPlusConfig {
 
-    @Bean("mybatisSqlSession")
-    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
-        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
-        MybatisConfiguration configuration = new MybatisConfiguration();
-        configuration.setDefaultScriptingLanguage(MybatisXMLLanguageDriver.class);
-        configuration.setJdbcTypeForNull(JdbcType.NULL);
-        configuration.setCallSettersOnNulls(true);
+//    @Bean("mybatisSqlSession")
+//    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+//        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
+//        MybatisConfiguration configuration = new MybatisConfiguration();
+//        configuration.setDefaultScriptingLanguage(MybatisXMLLanguageDriver.class);
+//        configuration.setJdbcTypeForNull(JdbcType.NULL);
+//        configuration.setCallSettersOnNulls(true);
+//
+//        //注册Map下划线转驼峰   此为方法一，也可在配置文件中配置
+//        configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
+//
+//        sqlSessionFactory.setDataSource(dataSource);
+//        sqlSessionFactory.setConfiguration(configuration);
+//
+////        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+////        ArrayList<ISqlParser> iSqlParsers = new ArrayList<>();
+////        TenantSqlParser tenantSqlParser = new TenantSqlParser();
+////        tenantSqlParser.setTenantHandler(new TenantHandler() {
+////            @Override
+////            public Expression getTenantId(boolean where) {
+////                return new LongValue(4546545455L);
+////            }
+////
+////            @Override
+////            public String getTenantIdColumn() {
+////                return "manager_id";
+////            }
+////
+////            @Override
+////            public boolean doTableFilter(String tableName) {
+////                if (tableName.equals("admin_info")){
+////                    return true;
+////                }
+////                return false;
+////            }
+////        });
+////        iSqlParsers.add(tenantSqlParser);
+////        paginationInterceptor.setSqlParserList(iSqlParsers);
+////        paginationInterceptor.setSqlParserFilter(new ISqlParserFilter() {
+////            @Override
+////            public boolean doFilter(MetaObject metaObject) {
+////                MappedStatement mappedStatement = SqlParserHelper.getMappedStatement(metaObject);
+////                if ("ciih.dsg.xhj.mapper.ClassInfoMapper.insert".equals(mappedStatement.getId())){
+////                    return true;
+////                }
+////                return false;
+////            }
+////        });
+//
+////        sqlSessionFactory.setPlugins(paginationInterceptor);
+//
+//        return sqlSessionFactory.getObject();
+//    }
 
-        //注册Map下划线转驼峰   此为方法一，也可在配置文件中配置
-        configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
-
-        sqlSessionFactory.setDataSource(dataSource);
-        sqlSessionFactory.setConfiguration(configuration);
-
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-//        ArrayList<ISqlParser> iSqlParsers = new ArrayList<>();
-//        TenantSqlParser tenantSqlParser = new TenantSqlParser();
-//        tenantSqlParser.setTenantHandler(new TenantHandler() {
-//            @Override
-//            public Expression getTenantId(boolean where) {
-//                return new LongValue(4546545455L);
-//            }
-//
-//            @Override
-//            public String getTenantIdColumn() {
-//                return "manager_id";
-//            }
-//
-//            @Override
-//            public boolean doTableFilter(String tableName) {
-//                if (tableName.equals("admin_info")){
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-//        iSqlParsers.add(tenantSqlParser);
-//        paginationInterceptor.setSqlParserList(iSqlParsers);
-//        paginationInterceptor.setSqlParserFilter(new ISqlParserFilter() {
-//            @Override
-//            public boolean doFilter(MetaObject metaObject) {
-//                MappedStatement mappedStatement = SqlParserHelper.getMappedStatement(metaObject);
-//                if ("ciih.dsg.xhj.mapper.ClassInfoMapper.insert".equals(mappedStatement.getId())){
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-
-        sqlSessionFactory.setPlugins(paginationInterceptor);
-
-        return sqlSessionFactory.getObject();
+        return paginationInterceptor;
     }
-
 }
